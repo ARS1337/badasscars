7a21ed945516a5b77fdee4d91770d4afbd1abd99
abhaySingh <38357472+ARS1337@users.noreply.github.com>
2022-05-18 02:44:06 +0530

    cityscape animation done, little work remaining

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -14,8 +14,8 @@ function MainPage(props) {
   const [buyNowClassList, setbuyNowClassList] = useState(
     " animate-buyNowAnimation absolute text-white rounded-full h-12 w-12 md:h-24 md:w-24 bg-[#292F33] z-50 border-2 top-[40%] md:top-[45%] md:border-8 border-[#EEFF00] flex items-center justify-center font-oswald           border-opacity-50 font-bold hover:opacity-50 hover:cursor-pointer  "
   );
-  const leftTyreBaseClasses = " absolute z-50  bottom-[12%] md:bottom-0 lg:bottom-0  left-[12.1%] ";
-  const rightTyreBaseClasses = " absolute z-50 bottom-[12%] md:bottom-1 lg:bottom-1 right-[10.5%] ";
+  const leftTyreBaseClasses = " absolute z-50  bottom-[11%] md:bottom-0 lg:bottom-0  left-[12.1%] ";
+  const rightTyreBaseClasses = " absolute z-50 bottom-[11%] md:bottom-1 lg:bottom-1 right-[10.5%] ";
 
   const [leftTyreAnimation, setleftTyreAnimation] = useState(leftTyreBaseClasses + " " + "animate-none");
   const [rightTyreAnimation, setrightTyreAnimation] = useState(rightTyreBaseClasses + " " + "animate-none");
@@ -26,15 +26,12 @@ function MainPage(props) {
     let newClassListString = baseClassList.join(" ") + " " + newClassToAdd;
     return newClassListString;
   };
-
   const setNewClass = (currCarClass, newCarClass, newCarNo, wait = 1000, classSetter, direction) => {
     //tyre animation
     if (direction === "left") {
-      setcityScapeClassList("animate-cityscape");
       setleftTyreAnimation(leftTyreBaseClasses + " " + "animate-[wheel_4s_linear_infinite_forwards]");
       setrightTyreAnimation(rightTyreBaseClasses + " " + "animate-[wheel_4s_linear_infinite_forwards]");
     } else if (direction === "right") {
-      setcityScapeClassList("animate-cityscapeReverse");
       setleftTyreAnimation(leftTyreBaseClasses + " " + "animate-[wheel_4s_linear_infinite_reverse_forwards]");
       setrightTyreAnimation(rightTyreBaseClasses + " " + "animate-[wheel_4s_linear_infinite_reverse_forwards]");
     }
@@ -55,6 +52,11 @@ function MainPage(props) {
         let newClassListString = returnNewClassList(carClassList, newCarClass);
         classSetter(newClassListString);
         setbuyNowClassList(buyNowClassList + "visible animate-onlyBounce z-50 ");
+        if (direction == "left") {
+          setcityScapeClassList("animate-cityscape");
+        } else {
+          setcityScapeClassList("animate-cityscapeReverse");
+        }
         setcurrentCar(newCarNo);
       }, [wait]);
     }
@@ -68,13 +70,12 @@ function MainPage(props) {
       className="overflow-hidden main-page scroll-area z-40  md:pt-[20vh] md:mb-36 md:h-screen main-page animate-secondPageTopToBottom bg-[#CED6DC]"
       key="main"
     >
-      <div className="font-roboto  " key={cityScapeClassList + currentCar + 99}>
+      <div className="font-roboto  ">
         <div id="sliding-background" className={cityScapeClassList} key={cityScapeClassList + currentCar + 99}></div>
         <div className=" md:min-h-[80vh] z-[1] overflow-hidden bg-newGrey ">
           <div className="flex items-center justify-center z-10">
             <Cars
               carDetails={configs?.carList[currentCar]}
-              key={currentCar + 8}
               currentCar={currentCar}
               carClassList={carClassList}
               setNewClass={setNewClass}
